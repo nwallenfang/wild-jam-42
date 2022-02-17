9197770004c87be18d893c7655ce2d8c61d5c41f
nwallenfang <nils@wallenfang.de>
2022-02-17 12:21:24 +0100

    House collisions and changes to PhysicsMover

--- Logic/PhysicsMover3D.gd
+++ Logic/PhysicsMover3D.gd
@@ -17,6 +17,9 @@ var snap_vector := Vector3.DOWN
 var up_vector := Vector3.UP
 
 
+var floor_max_angle = deg2rad(50.0)
+var max_slides = 4
+var stop_on_slopes = true
 
 
 func add_acceleration(var added_acc: Vector3):
@@ -65,7 +68,7 @@ func execute_movement(delta: float) -> void:
 		# to prevent long sliding down ramps
 		velocity = Vector3.ZERO
 		
-	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector3.UP)
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector3.UP, stop_on_slopes, max_slides, floor_max_angle)
 	
 	var just_landed = is_on_floor() and snap_vector == Vector3.ZERO
 	if just_landed:
