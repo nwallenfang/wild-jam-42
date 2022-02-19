71cfc54facfe2b1e00908ad24ac38d80dd684530
nwallenfang <nils@wallenfang.de>
2022-02-19 11:15:46 +0100

    pause and last few textr messages

--- Logic/Game.gd
+++ Logic/Game.gd
@@ -5,6 +5,9 @@ const selection_crosshair = preload("res://Assets/Sprites/cross_selection_circle
 const default_crosshair_black = preload("res://Assets/Sprites/crosshair_dark.png")
 onready var CROSSHAIR: TextureRect
 
+func _ready() -> void:
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
 var current = "default"
 func change_crosshair(name: String):
 	if name != current:
@@ -18,3 +21,14 @@ func change_crosshair(name: String):
 		current = name
 			
 			
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if Input.is_action_just_pressed("pause"):
+			UI.get_node("Center/PauseLabel").visible = not UI.get_node("Center/PauseLabel").visible
+			get_tree().paused = not get_tree().paused
+			if get_tree().paused:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			else:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
