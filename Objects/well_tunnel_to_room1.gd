89d6c65ab8871a644689275eb19897420dc8befb
nwallenfang <nils@wallenfang.de>
2022-02-20 18:04:18 +0100

    well and pyramid lighting

--- Objects/well_tunnel_to_room1.gd
+++ Objects/well_tunnel_to_room1.gd
@@ -4,5 +4,5 @@ extends Spatial
 
 func _on_Area_body_entered(body: Node) -> void:
 	if body is Player:
-		$Tween.interpolate_property($OmniLight, "light_energy", 0, .7, 1.5)
+		$Tween.interpolate_property($OmniLight, "light_energy", 0, .3, 1.5)
 		$Tween.start()
